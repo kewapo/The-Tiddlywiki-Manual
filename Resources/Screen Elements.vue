7d62e9b96c2e154ec1f28b56c1f4304c97c7ab9a
Kewapo <kewapo@gmail.com>
2019-10-11 13:20:26 +0200

    Chapter 1 first write. Chapter 2 first write.

---
+++ Resources/Screen Elements.vue
@@ -0,0 +1,276 @@
+<!-- Tufts VUE 3.3.0 concept-map (Screen Elements.vue) 2019-10-10 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Users/09767841V/Documents/VARIOS/VUE/VUE%203.3.0.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Thu Oct 10 10:18:14 CEST 2019 by 09767841V on platform Windows 10 10.0 in JVM 1.8.0_221-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built October 8 2015 at 1658 by tomadm on Linux 2.6.32-504.23.4.el6.x86_64 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="Screen Elements.vue" created="1570694541371" x="0.0" y="0.0"
+    width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1570695494123" size="11980"
+        spec="E:\tiddlywiki\The Tiddlywiki Manual\Resources\Screen Elements.vue"
+        type="1" xsi:type="URLResource">
+        <title>Screen Elements.vue</title>
+        <property key="File" value="E:\tiddlywiki\The Tiddlywiki Manual\Resources\Screen Elements.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9fb533e5b</URIString>
+    <child ID="4" layerID="1" created="1570694565678" x="143.0"
+        y="1.9033203" width="1048.0" height="771.20764"
+        strokeWidth="0.0" autoSized="false" xsi:type="image">
+        <resource referenceCreated="0" size="142870"
+            spec="E:\tiddlywiki\The Tiddlywiki Manual\Images\empty_screen.png"
+            type="1" xsi:type="URLResource">
+            <title>empty_screen.png</title>
+            <property key="Content.size" value="142870"/>
+            <property key="Content.modified" value="Thu Oct 10 10:01:44 CEST 2019"/>
+            <property key="image.width" value="1045"/>
+            <property key="image.height" value="769"/>
+            <property key="image.format" value="png"/>
+            <property key="File" value="E:\tiddlywiki\The Tiddlywiki Manual\Images\empty_screen.png"/>
+        </resource>
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#404040</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>-bold-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9b1c76ed4</URIString>
+    </child>
+    <child ID="5" label="Top right bar" layerID="1"
+        created="1570694592509" x="1036.0" y="81.0" width="72.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9710de9bf</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="6" label="Top left bar" layerID="1"
+        created="1570694656377" x="157.0" y="79.5" width="65.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae934e30929</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="7" label="Above Story" layerID="1"
+        created="1570694692794" x="493.0" y="76.5" width="71.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9cbd562cf</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="8" label="Below Story" layerID="1"
+        created="1570694712666" x="485.0" y="728.5" width="70.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9ccacd447</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="9" label="The search bar" layerID="1"
+        created="1570694724410" x="1105.0" y="256.5" width="93.0"
+        height="21.0" strokeWidth="2.0" autoSized="false" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9dd4cc279</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="10" label="The page buttons" layerID="1"
+        created="1570694736873" x="1105.0" y="220.5" width="94.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae983305f6c</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="11" label="The tiddler buttons" layerID="1"
+        created="1570694750841" x="625.0" y="156.5" width="101.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9f6531c39</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="12" label="The menus" layerID="1" created="1570694778474"
+        x="1106.0" y="295.5" width="92.0" height="21.0"
+        strokeWidth="2.0" autoSized="false" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae90ce0c9f0</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="13" label="Tiddler title" layerID="1"
+        created="1570694787914" x="393.0" y="170.5" width="65.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9cb438b0f</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="14" label="Tiddler content" layerID="1"
+        created="1570694807577" x="509.0" y="322.5" width="83.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9b70176bd</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="16" label="Tiddlywiki subtitle" layerID="1"
+        created="1570694852698" x="1105.0" y="184.5" width="99.0"
+        height="21.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae90b69b4b4</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="17" label="Tiddlywiki title" layerID="1"
+        created="1570694881033" x="1106.0" y="148.53351" width="94.0"
+        height="21.0" strokeWidth="2.0" autoSized="false" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae95abddb0c</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="22" label="The Navigation History&#xa;Zone" layerID="1"
+        created="1570695143066" x="462.0" y="657.50714" width="121.0"
+        height="34.0" strokeWidth="2.0" autoSized="true" xsi:type="node">
+        <fillColor>#FDE888</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4bf39fb0a241d29012feae9ddf3053a</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="23" layerID="1" created="1570695168218" x="178.0"
+        y="663.00714" width="11.0" height="21.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <fillColor>#EEEEEE</fillColor>
+        <strokeColor>#FFFFFF</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4bf39fb0a241d29012feae950f1ff50</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="24" layerID="1" created="1570695168842" x="883.0"
+        y="664.00714" width="11.0" height="21.0" strokeWidth="0.0"
+        autoSized="true" xsi:type="node">
+        <fillColor>#EEEEEE</fillColor>
+        <strokeColor>#FFFFFF</strokeColor>
+        <textColor>#AD0C03</textColor>
+        <font>Arial-bold-10</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4bf39fb0a241d29012feae97f3c4957</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="25" layerID="1" created="1570695250126" x="188.0"
+        y="672.5233" width="275.0" height="2.805359" strokeWidth="2.0"
+        autoSized="false" controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#AD0C03</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4bf39fb0a241d29012feae988020059</URIString>
+        <point1 x="462.0" y="674.3287"/>
+        <point2 x="189.0" y="673.5233"/>
+        <ID1 xsi:type="node">22</ID1>
+        <ID2 xsi:type="node">23</ID2>
+    </child>
+    <child ID="26" layerID="1" created="1570695255281" x="582.0"
+        y="673.50714" width="302.0" height="2.0" strokeWidth="2.0"
+        autoSized="false" controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#AD0C03</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-11</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/b4bf39fb0a241d29012feae9b8ae24f8</URIString>
+        <point1 x="583.0" y="674.50714"/>
+        <point2 x="883.0" y="674.50714"/>
+        <ID1 xsi:type="node">22</ID1>
+        <ID2 xsi:type="node">24</ID2>
+    </child>
+    <layer ID="1" label="Layer 1" created="1570694541371" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae927a9c696</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="129.0" y="-12.09668"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Camino sin t&#xed;tulo"
+            created="1570694541371" x="0.0" y="0.0" width="1.4E-45"
+            height="1.4E-45" strokeWidth="0.0" autoSized="false"
+            currentIndex="0" open="true">
+            <strokeColor>#B3CC33CC</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/b4b82de00a241d29012feae9664fb784</URIString>
+            <masterSlide ID="18" created="1570695032288" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/b4b82e0f0a241d29012feae9a438ac28</URIString>
+                <titleStyle ID="19" label="Header"
+                    created="1570695032320" x="335.5" y="172.5"
+                    width="129.0" height="55.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/b4b82e0f0a241d29012feae9f5d97e1c</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="20" label="Slide Text"
+                    created="1570695032320" x="346.5" y="281.5"
+                    width="107.0" height="37.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/b4b82e0f0a241d29012feae9705bbea6</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="21" label="Links" created="1570695032335"
+                    x="373.5" y="384.0" width="53.0" height="32.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/b4b82e0f0a241d29012feae9265ee843</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2019-10-10</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>E:\tiddlywiki\The Tiddlywiki Manual\Resources</saveLocation>
+    <saveFile>E:\tiddlywiki\The Tiddlywiki Manual\Resources\Screen Elements.vue</saveFile>
+</LW-MAP>
